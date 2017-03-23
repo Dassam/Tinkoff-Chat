d8526cc4321f404b3989bb5d98b0521e4d2b8b34
Яан Прокофьев <Dassam@dassam.local>
2017-03-23 23:23:07 +0300

    Changed the avatar menu work

--- TinkoffChat/TinkoffChat/ViewController.swift
+++ TinkoffChat/TinkoffChat/ViewController.swift
@@ -16,13 +16,24 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate,
     @IBOutlet weak var userImageAvatar: UIImageView!
     @IBOutlet weak var colorText: UILabel!
     
-    override func loadView() {
+    @IBAction func blackColorChange(_ sender: UIButton) { colorText.textColor = UIColor.black }
+    @IBAction func redColorChange(_ sender: UIButton) { colorText.textColor = UIColor.red }
+    @IBAction func greenColorChange(_ sender: UIButton) { colorText.textColor = UIColor.green }
+    @IBAction func blueColorChange(_ sender: UIButton) { colorText.textColor = UIColor.blue }
+    @IBAction func pinkColorChange(_ sender: UIButton) { colorText.textColor = UIColor.purple }
+    
+    @IBAction func saveAction(_ sender: Any) {print("Data is saved");}
+    
+    
+    override func loadView()
+    {
         super.loadView()
         print("loadView")
     }
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         print("viewDidLoad")
 
@@ -39,36 +50,17 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate,
 
     
     
-    func dismissKeyboard() {
-        userNameField.resignFirstResponder()
-    }
+    func dismissKeyboard() { userNameField.resignFirstResponder() }
     
     
 
-    func textFieldShouldReturn(_ userNameField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ userNameField: UITextField) -> Bool
+    {
         userNameField.resignFirstResponder()
         return true
     }
     
-    @IBAction func blackColorChange(_ sender: UIButton) {
-    colorText.textColor = UIColor.black
-    }
-    
-    @IBAction func redColorChange(_ sender: UIButton) {
-    colorText.textColor = UIColor.red
-    }
-    
-    @IBAction func greenColorChange(_ sender: UIButton) {
-    colorText.textColor = UIColor.green
-    }
-    
-    @IBAction func blueColorChange(_ sender: UIButton) {
-    colorText.textColor = UIColor.blue
-    }
-    
-    @IBAction func pinkColorChange(_ sender: UIButton) {
-    colorText.textColor = UIColor.purple
-    }
+  
     
     func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
     {
@@ -77,10 +69,32 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate,
         let image = UIImagePickerController()
         image.delegate = self
         
-        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
         
-        image.allowsEditing = false
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
         
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }else{
+                print("Camera not available")
+            }
+            
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+      
         self.present(image, animated: true)
         {
             //After it is complete
@@ -100,13 +114,17 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate,
         
         self.dismiss(animated: true, completion: nil)
     }
-
     
-       @IBAction func savexzAction(_ sender: Any)
-   {
-     //print("Data is saved");
-  }
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
 
 }
 
